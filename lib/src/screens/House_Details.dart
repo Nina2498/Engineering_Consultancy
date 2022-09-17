8863502a1e6f6de7e985cb5d73d8e1407439dd87
Nina2498 <shene1998m@gmail.com>
2022-09-17 13:08:26 +0300

    dashboard screen updated

--- lib/src/screens/House_Details.dart
+++ lib/src/screens/House_Details.dart
@@ -19,6 +19,7 @@ class _HouseDetailsState extends State<HouseDetails> {
 
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[300],
       appBar: AppBar(
         title: Text("House Cost Estimation"),
         backgroundColor: Color.fromARGB(255, 63, 58, 58),
@@ -36,20 +37,23 @@ class _HouseDetailsState extends State<HouseDetails> {
               style: TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
+                
               ),
               ),
             ),
-            // Area
+                // Area
                 Container(
-                 // margin: EdgeInsets.fromLTRB(0, 0, 250, 0),
+                  margin: EdgeInsets.fromLTRB(0, 0, 250, 0),
                   child: Text("Write the Area ",
                    style: TextStyle(
+                    color: (Color.fromARGB(170, 148, 19, 120)),
                    fontSize: 15,
                     fontWeight: FontWeight.bold),
+                    
                     ),
                   ),
                 Container(
-                  //padding: EdgeInsets.fromLTRB(15,5,100,5),
+                  padding: EdgeInsets.fromLTRB(15,2,100,2),
                   child: TextField(
                     controller: _areaController,
                     decoration: InputDecoration(
@@ -62,20 +66,22 @@ class _HouseDetailsState extends State<HouseDetails> {
                 ),
                 //Quality 
                 Container(
-                 // margin: EdgeInsets.fromLTRB(0, 0, 250, 0),
-                  child: Text("Write the Quality",
+                  margin: EdgeInsets.fromLTRB(0, 0, 200, 0),
+                  child: Text("Write the Quality" 
+                    " (Low:1000  Medium: 2000 High:3000 )",
                    style: TextStyle(
+                   color: (Color.fromARGB(170, 148, 19, 120)),
                    fontSize: 15,
                     fontWeight: FontWeight.bold),
                     ),
                   ),
                 Container(
-                //  padding: EdgeInsets.fromLTRB(15,5,100,5),
+                 padding: EdgeInsets.fromLTRB(15,2,100,2),
                   child: TextField(
                   controller: _qualityController,
                     
                     decoration: InputDecoration(
-                      hintText: "Quality",
+                      hintText: "Quality  ",
                       border: OutlineInputBorder(),
 
                     ),
@@ -85,15 +91,16 @@ class _HouseDetailsState extends State<HouseDetails> {
 
                 //Number of Floors
                 Container(
-                 // margin: EdgeInsets.fromLTRB(0, 0, 210, 0),
+                  margin: EdgeInsets.fromLTRB(0, 0, 210, 0),
                   child: Text("Write Number of Floors",
                    style: TextStyle(
+                   color: (Color.fromARGB(170, 148, 19, 120)),
                    fontSize: 15,
                     fontWeight: FontWeight.bold),
                     ),
                   ),
                 Container(
-                 // padding: EdgeInsets.fromLTRB(15,5,100,5),
+                  padding: EdgeInsets.fromLTRB(15,5,100,5),
                   child: TextField(
                     controller: _numberoffloController,
                     decoration: InputDecoration(
@@ -105,10 +112,15 @@ class _HouseDetailsState extends State<HouseDetails> {
                   ),
                 ),
                 Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+            //mainAxisAlignment: MainAxisAlignment.center,
+            
             children: <Widget>[
-              RaisedButton(
-                child: Text("Caculate"),
+              ElevatedButton(
+              style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(170, 148, 19, 120))
+                            ),
+                            
+                child: Text("Calculate"),
                 onPressed : () {
                   setState(() {
                     int result = int.parse(_areaController.text) * int.parse(_qualityController.text) *int.parse(_numberoffloController.text);
@@ -116,23 +128,34 @@ class _HouseDetailsState extends State<HouseDetails> {
                   });
                 },
               ),
-                
-              ],
-          
-            ),
-            Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text("Result:",
+                 Text("Result:",
                 style: TextStyle(
                     fontSize: 30,
                 ),),
                new Text(resulttext,
                  style: TextStyle(
                   fontSize: 30,
-               ),),
-            ],
-          ),
+               ),
+               ),
+              ],
+          
+            ),
+
+
+          //   Row(
+          //   mainAxisAlignment: MainAxisAlignment.center,
+          //   children: <Widget>[
+          //     Text("Result:",
+          //       style: TextStyle(
+          //           fontSize: 30,
+          //       ),),
+          //      new Text(resulttext,
+          //        style: TextStyle(
+          //         fontSize: 30,
+          //      ),
+          //      ),
+          //   ],
+          // ),
         
           
     ]
